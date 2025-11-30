98d9e08f92bfdb9b87fe146387f03c02360416c2
hemasree-ship-it <alakamsettyhemasree@gmail.com>
2025-11-30 21:31:36 +0530

    Update event listener for adding new cards

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// This makes the "Add New Item" button create a new card
-document.querySelector('button').addEventListener('click', function() {
+document.getElementById("addItemBtn").addEventListener("click", function() {
     const container = document.querySelector('.container');
 
-    // Create a new card
     const newCard = document.createElement('div');
     newCard.className = 'card';
     newCard.innerHTML = `
         <h3>Lost: <span style="color:#ffcc00;">New Item</span></h3>
         <p>Location: <span style="color:#00ccff;">Unknown</span></p>
-        <p>Contact: 000-000-0000</span></p>
+        <p>Contact: 000-000-0000</p>
     `;
 
-    // Add it to the page
     container.appendChild(newCard);
 });
