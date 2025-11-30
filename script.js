eaaaffdbf56213564110f368f0d558f5d61dd83b
hemasree-ship-it <alakamsettyhemasree@gmail.com>
2025-11-30 21:36:19 +0530

    Enable content editing for new cards
    Made card content editable for user input.

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById("addItemBtn").addEventListener("click", function() {
     const newCard = document.createElement('div');
     newCard.className = 'card';
     newCard.innerHTML = `
-        <h3>Lost: <span style="color:#ffcc00;">New Item</span></h3>
-        <p>Location: <span style="color:#00ccff;">Unknown</span></p>
-        <p>Contact: 000-000-0000</p>
+        <h3 contenteditable="true">Lost: <span style="color:#ffcc00;">New Item</span></h3>
+        <p contenteditable="true">Location: <span style="color:#00ccff;">Unknown</span></p>
+        <p contenteditable="true">Contact: 000-000-0000</p>
     `;
 
     container.appendChild(newCard);
